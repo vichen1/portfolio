eaa7cf2d0b00636fbd3de90374da7cc3a00fa002
vichen1 <vic006@ucsd.edu>
2025-10-18 19:04:29 -0700

    nav changed

--- global.js
+++ global.js
@@ -1,13 +1,41 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "resume/", title: "CV" },
+  { url: "contact/", title: "Contact" },
+  { url: "https://github.com/vichen1", title: "GitHub" }
+];
 
-let currentLink = navLinks.find(
-  a => a.host === location.host && a.pathname === location.pathname
-);
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/your-repo-name/";
 
-currentLink?.classList.add("current");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url.startsWith("http") ? p.url : BASE_PATH + p.url;
+  let title = p.title;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}

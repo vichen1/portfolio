93bc59e4aa3310b9ffb927a239fe3545dd26bc06
vichen1 <vic006@ucsd.edu>
2025-10-18 19:28:57 -0700

    Update global.js

--- global.js
+++ global.js
@@ -36,3 +36,20 @@ for (let p of pages) {
 
   nav.append(a);
 }
+document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="theme">
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+  
+  document.querySelector("#theme").addEventListener("change", (e) => {
+    document.documentElement.style.colorScheme = e.target.value;
+  });

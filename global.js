3e4533fb099770debd4298d869f6b0f14245a7e9
vichen1 <vic006@ucsd.edu>
2025-10-24 16:00:04 -0700

    1.2

--- global.js
+++ global.js
@@ -69,3 +69,16 @@ select.addEventListener("input", (event) => {
   console.log("Color scheme changed to:", event.target.value);
   setColorScheme(event.target.value);
 });
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}

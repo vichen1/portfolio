dc099260a1b187aaf15718568a61418f3fa7eb19
vichen1 <vic006@ucsd.edu>
2025-10-24 16:05:21 -0700

    1.6

--- projects/projects.js
+++ projects/projects.js
@@ -3,3 +3,8 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+const titleElement = document.querySelector('.projects-title');
+if (titleElement && Array.isArray(projects)) {
+  titleElement.textContent = `${projects.length} Projects`;
+}

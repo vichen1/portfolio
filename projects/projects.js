23c1868c0e496d005903d31eea7bfc57f6493ae2
vichen1 <vic006@ucsd.edu>
2025-10-30 15:53:40 -0700

    chart fixed

--- projects/projects.js
+++ projects/projects.js
@@ -8,3 +8,10 @@ const titleElement = document.querySelector('.projects-title');
 if (titleElement && Array.isArray(projects)) {
   titleElement.textContent = `${projects.length} Projects`;
 }
+const svg = d3.select("#projects-plot");
+
+svg.append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 50)
+  .attr("fill", "red");

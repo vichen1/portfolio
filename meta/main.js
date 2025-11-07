ecd0ee1f5039cfebf61b4e971edfb4a095ae72ae
vichen1 <vic006@ucsd.edu>
2025-11-07 13:58:39 -0800

    step 1 done

--- meta/main.js
+++ meta/main.js
@@ -9,37 +9,68 @@ async function loadData() {
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
-
   return data;
 }
 
 function processCommits(data) {
-    return d3
-      .groups(data, (d) => d.commit)
-      .map(([commit, lines]) => {
-        let first = lines[0];
-        let { author, date, time, timezone, datetime } = first;
-          let ret = {
-          id: commit,
-          url: 'https://github.com/vichen1/portfolio/commit/' + commit,
-          author,
-          date,
-          time,
-          timezone,
-          datetime,
-          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-          totalLines: lines.length,
-        };
-  
-        Object.defineProperty(ret, 'lines', {
-          value: lines,
-          enumerable: false, 
-        });
-  
-        return ret;
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      const first = lines[0];
+      const { author, date, time, timezone, datetime } = first;
+
+      const ret = {
+        id: commit,
+        url: 'https://github.com/vichen1/portfolio/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,
       });
-  }
-  
+
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  const numFiles = d3.group(data, (d) => d.file).size;
+  dl.append('dt').text('Files');
+  dl.append('dd').text(numFiles);
+
+  const maxDepth = d3.max(data, (d) => d.depth);
+  dl.append('dt').text('Max depth');
+  dl.append('dd').text(maxDepth);
+
+  const longestLine = d3.max(data, (d) => d.length);
+  dl.append('dt').text('Longest line');
+  dl.append('dd').text(longestLine);
+
+  const fileLengths = d3.rollups(
+    data,
+    (v) => d3.max(v, (d) => d.line),
+    (d) => d.file
+  );
+  const avgFileLength = d3.mean(fileLengths, (d) => d[1]);
+  dl.append('dt').text('Avg file length');
+  dl.append('dd').text(avgFileLength.toFixed(1));
+}
+
 let data = await loadData();
 let commits = processCommits(data);
-console.log(commits);
+renderCommitInfo(data, commits);
